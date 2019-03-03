ac3e3864d47501462b91e2d6010f6fefd58856bc
classy-debugger-3131 <elle.valentine@hotmail.com>
2019-03-03 16:50:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-var animal = 'dog'
+var animal = 'dog';
 
 function myAnimal() {
-  return animal
+  return animal;
 }
 
 function yourAnimal() {
- var animal=  'cat' 
-  return animal
+ var animal=  'cat' ;
+  return animal;
 }
 
 function add2(n) {
-   const two = 2
+   const two = 2;
 
-  return parseInt(n, 10) + two
+  return parseInt(n, 10) + two;
 }
 
 var funkyFunction = function() {
   return function() {
-    return "FUNKY!"
-  }
-}
+    return "FUNKY!";
+  };
+};
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction()
-theFunk= theFunk()
+
+var theFunk = funkyFunction();
+theFunk= theFunk();
